36070da7f0e9a8703352d17c2460b73aa9b87613
ytantawy1 <113364714+ytantawy1@users.noreply.github.com>
2024-02-10 22:03:36 -0700

    Add files to repository

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 import { getToDoData } from "./lib/firebase/api";
 
 // single source of truth for the data...
-let store = [];
+
 async function appInit() {
     const toDos = await getToDoData();
     console.log(toDos);
