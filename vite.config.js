1652fcfe973d444ee153b85f0b2e093a993d9049
ytantawy1 <113364714+ytantawy1@users.noreply.github.com>
2024-02-10 21:36:54 -0700

    Add files to repository

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,24 @@
-import { defineConfig } from 'vite';
-import { createVuePlugin } from 'vite-plugin-vue';
-import { resolve } from 'path';
+import { resolve } from "path";
+import { defineConfig } from "vite-plugin-vue";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: 3000,
-  },
-  resolve: {
-    alias: {
-      "@": "./src",
-    },
-  },
-  build: {
-    rollupOptions: {
-      input: {
-        index: resolve(__dirname, "./index.html"),
-        demo: resolve(__dirname, "./live-demo.html"),
-        login: resolve(__dirname, "./login.html"),
-        user: resolve(__dirname, "./user.html"),
-      },
-    },
-  },
-  plugins: [createVuePlugin()],
-  esbuild: {
-    jsxFactory: 'h',
-    jsxFragment: 'Fragment',
-  },
+	server: {
+		port: 3000,
+	},
+	resolve: {
+		alias: {
+			"@": "./src",
+		},
+	},
+	build: {
+		rollupOptions: {
+			input: {
+				index: resolve(__dirname, "./index.html"),
+				demo: resolve(__dirname, "./live-demo.html"),
+				login: resolve(__dirname, "./login.html"),
+				user: resolve(__dirname, "./user.html"),
+			},
+		},
+	},
 });
